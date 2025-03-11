d570f1566a766a791d903b2782608a550f3cae40
Peter André Altier <petera113.pa@gmail.com>
2025-03-11 13:02:48 +0100

    yes

--- model.js
+++ model.js
@@ -1,8 +1,25 @@
 const answerList = [
-    { answer: "yes" },
-    { answer: "no" },
-    { answer: "maybe" },
-    { answer: "definitely" }
+    {answer:'It is certain'},
+    {answer:'Reply hazy'},
+    {nswer:'try again'},
+    {answer:'Don’t count on it'},
+    {answer:'It is decidedly so'},
+	{answer:'Ask again later'},
+    {answer:'My reply is no'},
+    {answer:'Without a doubt'},	
+    {answer:'Better not tell you now'},
+	{answer:'My sources say no'},
+    {answer:'Yes definitely'},
+    {answer:'Cannot predict now'},	
+    {answer:'Outlook not so good'},
+    {answer:'You may rely on it'},
+    {answer:'Concentrate and ask again'},
+    {answer:'Very doubtful'},
+    {answer:'As I see it, yes'},	
+    {answer:'Most likely'},		
+    {answer:'Outlook good'},		
+    {answer:'Yes'},		
+    {answer:'Signs point to yes'}	
 ];
 
 let inputText = "";
